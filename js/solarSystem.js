95436313cd2a38a89d9be07256e6e849d5c31bef
Ankit kumar jha <ankitjha2603@gmail.com>
2023-07-26 21:38:26 +0530

    Add files via upload

---
+++ js/solarSystem.js
@@ -0,0 +1,256 @@
+//Import
+import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";
+import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
+//////////////////////////////////////
+//NOTE Creating renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE texture loader
+const textureLoader = new THREE.TextureLoader();
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE import all texture
+const starTexture = textureLoader.load("./image/stars.jpg");
+const sunTexture = textureLoader.load("./image/sun.jpg");
+const mercuryTexture = textureLoader.load("./image/mercury.jpg");
+const venusTexture = textureLoader.load("./image/venus.jpg");
+const earthTexture = textureLoader.load("./image/earth.jpg");
+const marsTexture = textureLoader.load("./image/mars.jpg");
+const jupiterTexture = textureLoader.load("./image/jupiter.jpg");
+const saturnTexture = textureLoader.load("./image/saturn.jpg");
+const uranusTexture = textureLoader.load("./image/uranus.jpg");
+const neptuneTexture = textureLoader.load("./image/neptune.jpg");
+const plutoTexture = textureLoader.load("./image/pluto.jpg");
+const saturnRingTexture = textureLoader.load("./image/saturn_ring.png");
+const uranusRingTexture = textureLoader.load("./image/uranus_ring.png");
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE Creating scene
+const scene = new THREE.Scene();
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE screen bg
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const cubeTexture = cubeTextureLoader.load([
+  starTexture,
+  starTexture,
+  starTexture,
+  starTexture,
+  starTexture,
+  starTexture,
+]);
+scene.background = cubeTexture;
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE Perspective Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(-50, 90, 150);
+////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE Percpective controll
+const orbit = new OrbitControls(camera, renderer.domElement);
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE - sun
+const sungeo = new THREE.SphereGeometry(15, 50, 50);
+const sunMaterial = new THREE.MeshBasicMaterial({
+  map: sunTexture,
+});
+const sun = new THREE.Mesh(sungeo, sunMaterial);
+scene.add(sun);
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE - sun light (point light)
+const sunLight = new THREE.PointLight(0xffffff, 4, 300);
+scene.add(sunLight);
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE - ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0);
+scene.add(ambientLight);
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE - path for planet
+const path_of_planets = [];
+function createLineLoopWithMesh(radius, color, width) {
+  const material = new THREE.LineBasicMaterial({
+    color: color,
+    linewidth: width,
+  });
+  const geometry = new THREE.BufferGeometry();
+  const lineLoopPoints = [];
+
+  // Calculate points for the circular path
+  const numSegments = 100; // Number of segments to create the circular path
+  for (let i = 0; i <= numSegments; i++) {
+    const angle = (i / numSegments) * Math.PI * 2;
+    const x = radius * Math.cos(angle);
+    const z = radius * Math.sin(angle);
+    lineLoopPoints.push(x, 0, z);
+  }
+
+  geometry.setAttribute(
+    "position",
+    new THREE.Float32BufferAttribute(lineLoopPoints, 3)
+  );
+  const lineLoop = new THREE.LineLoop(geometry, material);
+  scene.add(lineLoop);
+  path_of_planets.push(lineLoop);
+}
+//////////////////////////////////////
+
+/////////////////////////////////////
+//NOTE: create planet
+const genratePlanet = (size, planetTexture, x, ring) => {
+  const planetGeometry = new THREE.SphereGeometry(size, 50, 50);
+  const planetMaterial = new THREE.MeshStandardMaterial({
+    map: planetTexture,
+  });
+  const planet = new THREE.Mesh(planetGeometry, planetMaterial);
+  const planetObj = new THREE.Object3D();
+  planet.position.set(x, 0, 0);
+  if (ring) {
+    const ringGeo = new THREE.RingGeometry(
+      ring.innerRadius,
+      ring.outerRadius,
+      32
+    );
+    const ringMat = new THREE.MeshBasicMaterial({
+      map: ring.ringmat,
+      side: THREE.DoubleSide,
+    });
+    const ringMesh = new THREE.Mesh(ringGeo, ringMat);
+    planetObj.add(ringMesh);
+    ringMesh.position.set(x, 0, 0);
+    ringMesh.rotation.x = -0.5 * Math.PI;
+  }
+  scene.add(planetObj);
+
+  planetObj.add(planet);
+  createLineLoopWithMesh(x, 0xffffff, 3);
+  return {
+    planetObj: planetObj,
+    planet: planet,
+  };
+};
+
+const planets = [
+  {
+    ...genratePlanet(3.2, mercuryTexture, 28),
+    rotaing_speed_around_sun: 0.004,
+    self_rotation_speed: 0.004,
+  },
+  {
+    ...genratePlanet(5.8, venusTexture, 44),
+    rotaing_speed_around_sun: 0.015,
+    self_rotation_speed: 0.002,
+  },
+  {
+    ...genratePlanet(6, earthTexture, 62),
+    rotaing_speed_around_sun: 0.01,
+    self_rotation_speed: 0.02,
+  },
+  {
+    ...genratePlanet(4, marsTexture, 78),
+    rotaing_speed_around_sun: 0.008,
+    self_rotation_speed: 0.018,
+  },
+  {
+    ...genratePlanet(12, jupiterTexture, 100),
+    rotaing_speed_around_sun: 0.002,
+    self_rotation_speed: 0.04,
+  },
+  {
+    ...genratePlanet(10, saturnTexture, 138, {
+      innerRadius: 10,
+      outerRadius: 20,
+      ringmat: saturnRingTexture,
+    }),
+    rotaing_speed_around_sun: 0.0009,
+    self_rotation_speed: 0.038,
+  },
+  {
+    ...genratePlanet(7, uranusTexture, 176, {
+      innerRadius: 7,
+      outerRadius: 12,
+      ringmat: uranusRingTexture,
+    }),
+    rotaing_speed_around_sun: 0.0004,
+    self_rotation_speed: 0.03,
+  },
+  {
+    ...genratePlanet(7, neptuneTexture, 200),
+    rotaing_speed_around_sun: 0.0001,
+    self_rotation_speed: 0.032,
+  },
+  {
+    ...genratePlanet(2.8, plutoTexture, 216),
+    rotaing_speed_around_sun: 0.0007,
+    self_rotation_speed: 0.008,
+  },
+];
+
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE - GUI options
+var GUI = dat.gui.GUI;
+const gui = new GUI();
+const options = {
+  "Real view": true,
+  "Show path": true,
+  speed: 1,
+};
+gui.add(options, "Real view").onChange((e) => {
+  ambientLight.intensity = e ? 0 : 0.5;
+});
+gui.add(options, "Show path").onChange((e) => {
+  path_of_planets.forEach((dpath) => {
+    dpath.visible = e;
+  });
+});
+gui.add(options, "speed", 0, 20);
+
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE - animate function
+function animate(time) {
+  sun.rotateY(options.speed * 0.004);
+  planets.forEach(
+    ({ planetObj, planet, rotaing_speed_around_sun, self_rotation_speed }) => {
+      planetObj.rotateY(options.speed * rotaing_speed_around_sun);
+      planet.rotateY(options.speed * self_rotation_speed);
+    }
+  );
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
+//////////////////////////////////////
+
+//////////////////////////////////////
+//NOTE - resize camera view
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+//////////////////////////////////////
---
+++ readme.md
@@ -0,0 +1,36 @@
+# ![Solar System](./image/solar-system.png) Solar System 3D Visualization using Three.js
+
+Explore the wonders of our Solar System in an immersive 3D experience! Witness the rotation of planets around the Sun and their self-rotation in this interactive web-based application developed using Three.js.
+
+![](./gif/display.gif)
+
+## Table of Contents
+
+1. [Introduction](#1-introduction)
+2. [Project Overview](#2-project-overview)
+3. [Features](#5-features)
+
+## 1. Introduction
+
+The Solar System 3D Visualization project is an interactive web-based application that allows users to explore the Solar System in a three-dimensional environment. The project is developed using the Three.js library, which provides powerful tools for creating 3D content on the web. Users can witness the rotation of planets around the Sun and their self-rotation, as well as enjoy the beauty of space in this immersive experience.
+
+## 2. Project Overview
+
+The project consists of an HTML file that sets up the web page, loads required libraries, and defines the basic structure. The main JavaScript file, `solarSystem.js`, contains the code that creates the 3D scene, loads textures for planets and space, and handles the animation and interactivity.
+
+## 3. Features
+
+- Realistic 3D visualization of the Solar System.
+- Interactive controls to explore the planets and their orbits.
+
+#### Speed Control
+
+You can adjust the animation speed to your preference. The `speed` slider in the GUI allows you to control the rate at which the planets rotate and move around the Sun. Slide it to the right to speed up the animation and to the left to slow it down.
+
+#### Show/Hide Planet Paths
+
+The application provides an option to show or hide the paths of the planets as they orbit around the Sun. By enabling the "Show path" toggle in the GUI, you can visualize the trajectories of the planets. Disabling it will hide the paths, allowing for a cleaner view of the Solar System.
+
+#### Realistic Lighting
+
+The "Real view" option in the GUI replicates the conditions in space, where parts of the planets facing away from the Sun are not illuminated and appear darker. When enabled, the application adjusts the lighting to create a more realistic representation of the planets' appearance.
